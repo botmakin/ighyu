e8d75337d30c58b8ff086179178337a7a27f6c2a
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2022-12-11 11:58:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,9 +134,9 @@ async def message_handler(event):
         )
         message = f'**Click Here 👇🏻 For "{event.text}"**\n\n[🌹🎬 {str(event.text).upper()}\n🌹🎬 {str("Click Me For Results").upper()}]({tgraph_result})'
 
-        newbutton = [
-                InlineKeyboardButton('🎃 Join Updates Channel 🎃', url='https://t.me/movies_halt')
-            ]
+        reply_markup=InlineKeyboardMarkup([
+            InlineKeyboardButton('🎃 Join Updates Channel 🎃', url=f'http://t.me/movies_halt')
+            ])
         await txt.delete()
         result = await event.reply(message, link_preview=False)
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
