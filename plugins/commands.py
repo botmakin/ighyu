76d54c710b392c2c2924c2741dabd707ba6605ca
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2022-12-12 15:29:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,10 +9,15 @@ from TeamTeleRoid.database import db
 async def help_handler(_, event: Message):
     await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
+            [
             InlineKeyboardButton('↪️ Add Me To Your Groups ↩️', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
-            ],[InlineKeyboardButton("🧩 About 🧩", callback_data="About_msg"),
-             InlineKeyboardButton("🎭Help 🎭", callback_data="Help_msg")]
-)                   
+            ],
+
+             [InlineKeyboardButton("🧩 About 🧩", callback_data="About_msg"),
+             InlineKeyboardButton("🎭 Help 🎭", callback_data="Help_msg")
+             ]
+        ])
+    )                        
 
 @Client.on_message(filters.command("total_users") & filters.private &  filters.chat(Config.BOT_OWNER))
 async def total_users(_, event: Message):
@@ -29,14 +34,14 @@ async def start_handler(_,event: Message):
         photo=Config.START_PHOTO,
         caption=Config.START_MSG.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-            InlineKeyboardButton('↪️ Add Me To Your Group  ↩️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton("🌹 Subscribe 🌹", url="https://t.me/movies_halt"),
-            InlineKeyboardButton("🤖 Updates 🤖", url="https://t.me/movies_halt_update")
-            ],[
-            InlineKeyboardButton("🎭 Help 🎭", callback_data="Help_msg"),
-            InlineKeyboardButton("🧩 About 🧩", callback_data="About_msg")
-        ]
+            [
+            InlineKeyboardButton('↪️ Add Me To Your Groups ↩️', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
+            ],
+
+             [InlineKeyboardButton("🧩 About 🧩", callback_data="About_msg"),
+             InlineKeyboardButton(" 🎭Help 🎭", callback_data="Help_msg")
+             ]
+        ])
     )
 
 VERIFY = {}
